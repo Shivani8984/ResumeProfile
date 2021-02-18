c0d243f4a73f82be36511a815ec3a181bed15810
Shivani8984 <67438875+Shivani8984@users.noreply.github.com>
2021-02-18 16:03:00 -0500

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -2,3 +2,4 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','fred', 'zap');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
